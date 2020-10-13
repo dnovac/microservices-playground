af328186abafe1e1d4b8c9bd65ad0c0d3d7b7542
Dan Novac <danynovac@gmail.com>
2020-10-14 00:02:39 +0300

    New Eureka Discovery Service added

--- customer-service/src/main/java/com/dnovac/customers/web/CustomersController.java
+++ customer-service/src/main/java/com/dnovac/customers/web/CustomersController.java
@@ -1,10 +1,15 @@
 package com.dnovac.customers.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 /**
@@ -16,5 +21,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("customers")
 public class CustomersController {
 
+  private final DiscoveryClient discoveryClient;
+
+  @RequestMapping("/service-instances/{applicationName}")
+  public List<ServiceInstance> serviceInstancesByApplicationName(
+          @PathVariable String applicationName) {
+    return this.discoveryClient.getInstances(applicationName);
+  }
 
 }
--- library-service/src/main/java/com/dnovac/library/LibraryServiceApplication.java
+++ library-service/src/main/java/com/dnovac/library/LibraryServiceApplication.java
@@ -3,13 +3,15 @@ package com.dnovac.library;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 /**
  * @author Dan Novac on 13/10/2020
  * @project microservices-playground
  */
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+@SpringBootApplication
 public class LibraryServiceApplication {
 
   public static void main(String[] args) {
